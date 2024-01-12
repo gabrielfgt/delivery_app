002e9694e46c526d68c3609076298467664bb159
Gabriel Teixeira <gabrielfgonsalves@gmail.com>
2024-01-12 10:29:50 -0300

    initial commit

---
+++ DeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryAppApp.swift
+//  DeliveryApp
+//
+//  Created by Gabriel Francisco on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
